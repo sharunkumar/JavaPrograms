94b5b3a0f1a97e72cb047babc91b5e64a5b484df
Sharun <sharunksplus@gmail.com>
2023-02-11 15:15:45 -0500

    added TaskScheduling

---
+++ src/main/java/com/sharunkumar/TaskScheduling.java
@@ -0,0 +1,51 @@
+package com.sharunkumar;
+
+import java.util.*;
+
+class Task {
+    int cost;
+    int time;
+}
+
+public class TaskScheduling {
+    public static int solve(List<Integer> costs, List<Integer> times) {
+        List<Task> tasks = new ArrayList<>();
+        for (int i = 0 ; i < costs.size() ; ++i) {
+            Task t = new Task();
+            t.cost = costs.get(i);
+            t.time = times.get(i);
+            tasks.add(t);
+        }
+
+        tasks.sort((t1, t2) -> {
+            if (t1.cost == t2.cost) {
+                return t2.time - t1.time;
+            }
+            return t1.cost - t2.cost;
+        });
+
+        int i = 0;
+        int j = tasks.size() - 1;
+        int totalCost = 0;
+        if (i == j && i == 0) {
+            return tasks.get(0).cost;
+        }
+        while (i < j) {
+            totalCost += tasks.get(i).cost;
+            int p = tasks.get(i).time;
+            while (p-- > 0 && i < j) {
+                j--;
+            }
+            i++;
+        }
+        return totalCost;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solve(Arrays.asList(1,1,3,4), Arrays.asList(3,1,2,3)));
+        System.out.println(solve(Arrays.asList(1,2,3,2), Arrays.asList(1,2,3,2)));
+        System.out.println(solve(Arrays.asList(2,3,4,2), Arrays.asList(1,1,1,1)));
+        System.out.println(solve(List.of(4), List.of(2)));
+    }
+}
+
